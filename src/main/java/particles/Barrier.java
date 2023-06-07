6f191ec16304b528b88e28f56f41af85a831d6dd
Alex <alexandercastronovo@gmail.com>
2023-06-07 01:04:21 -0400

    Make barrier black

--- src/main/java/particles/Barrier.java
+++ src/main/java/particles/Barrier.java
@@ -4,7 +4,7 @@ import game.Particles;
 
 public class Barrier extends Particle {
     public Barrier(int x, int y) {
-        super(x, y, new float[]{0, 0, 0, 0});
+        super(x, y, new float[]{0, 0, 0, 1});
     }
 
     @Override
